1fbcadf39d6f31479da7a9537290f283f1547e25
MKlepanosau <@mikita.klepanosau>
2023-02-23 17:11:18 +0100

    task-3

---
+++ product-service/src/mocks/books.js
@@ -0,0 +1,17 @@
+module.exports.BOOKS = [
+    {
+        name: 'Book 1',
+        price: 10,
+        id: 1,
+    },
+    {
+        name: 'Book 2',
+        price: 15,
+        id: 2,
+    },
+    {
+        name: 'Book 3',
+        price: 4,
+        id: 3,
+    },
+];
